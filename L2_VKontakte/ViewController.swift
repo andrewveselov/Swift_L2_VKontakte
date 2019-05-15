bbb4877f2ad7bc69fab83c3ccf0111c1d7efbc3b
andrew veselov <andrew.veselov@gmail.com>
2019-05-15 12:30:51 +0500

    Initial Commit

---
+++ L2_VKontakte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L2_VKontakte
+//
+//  Created by Andrew Veselov on 15/05/2019.
+//  Copyright © 2019 Andrew Veselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
