aac4d3cee6ad48bfe7956aed832c5af5b192df6b
andrew veselov <andrew.veselov@gmail.com>
2019-06-07 17:30:24 +0500

    Lesson 6. Homework task 1 complete

--- L2_VKontakte/FriendsListTableViewController.swift
+++ L2_VKontakte/FriendsListTableViewController.swift
@@ -5,63 +5,127 @@
 //  Created by Andrew Veselov on 20/05/2019.
 //  Copyright © 2019 Andrew Veselov. All rights reserved.
 //
-// Swift level 2 Lesson 4 2019-05-24
+// Swift level 2 Lesson 6 2019-06-04
 // Homework
 // Andrew Veselov
 //
-// 1. Создать свой View для аватарки. Он должен состоять из двух subview:
-// 1) Должен содержать UIImageView с фотографией пользователя и быть круглой формы.
-// 2) Должен находиться ниже и давать тень по периметру фотографии. Учтите, что тень будет отображена, если backgroudColor != .clear.
-// 2. Предусмотреть возможность изменения ширины, цвета, прозрачности тени в interface builder. (задание на самостоятельный поиск решения).
-// 3. Создать контрол «Мне нравится», с помощью которого можно поставить лайк под постом в ленте. Данный контрол должен объединять кнопку с иконкой сердца и количеством отметок рядом с ней. При нажатии на контрол нужно увеличить количество отметок, а при повторном нажатии — уменьшить (как это реализовано в приложении ВКонтакте). В состоянии, когда отметка поставлена, иконка и текст должны менять цвет.
-// 4 *Сделать контрол, позволяющий выбрать букву алфавита. Он понадобится на экране списка друзей. Дизайн можно позаимствовать у оригинального приложения ВКонтакте. Должна быть возможность выбрать букву нажатием или перемещением пальца по контролу. При выборе нужно пролистывать список к первому человеку, у которого фамилия начинается на эту букву. Желательно сделать так, чтобы в этом контроле не было букв, на которые не начинается ни одна фамилия друзей из списка. (необязательное, для тех, у кого есть время)
+// 1. Сделать группировку друзей по первой букве фамилии. Добавить header секции для таблицы со списком друзей. Он должен содержать первую букву фамилии и иметь полупрозрачный цвет фона, цвет которого совпадает с цветом таблицы.
+// 2. Добавьте UISearchBar в header таблицы со списком друзей и групп. Укажите контроллер, содержащий эту таблицу, делегатом UISearchBar, реализуйте поиск с выводом результатов в ту же таблицу. Для простоты реализации не стоит использовать UISearchController (задание на самостоятельный поиск решения).
+// 3. Создать экран новостей. Добавить туда таблицу и сделать ячейку для новости. Ячейка должна содержать то же самое, что и в оригинальном приложении ВКонтакте: надпись, фотографии, кнопки «Мне нравится», «Комментировать», «Поделиться» и индикатор количества просмотров. Сделать поддержку только одной фотографии, которая должна быть квадратной формы и растягиваться на всю ширину ячейки. Высота ячейки должна вычисляться автоматически.
+// 4. *В ячейку новости добавить отображение нескольких фотографий. Они должны располагаться в квадратной зоне, ширина которой равна ширине ячейки. В идеале нужно сделать равномерное расположение фотографий в квадратной области (необязательное, для тех, у кого есть время).
 //
 
 import UIKit
 
-class FriendsListTableViewController: UITableViewController {
+class FriendsListTableViewController: UITableViewController, UISearchBarDelegate {
     
     var friendsLocalUser : [UserModel] = [
         UserModel(name: "Иван Исаев", gender: .male, avatar: UIImage(named: "avatar1")!),
         UserModel(name: "Кристина Петрова", gender: .female, avatar: UIImage(named: "avatar2")!),
-        UserModel(name: "Алена", gender: .female, avatar: UIImage(named: "avatar3")!)
+        UserModel(name: "Алена", gender: .female, avatar: UIImage(named: "avatar3")!),
+        UserModel(name: "Рита Иванова", gender: .female, avatar: UIImage(named: "avatar2")!),
+        UserModel(name: "Марина Тихомирова", gender: .female, avatar: UIImage(named: "avatar2")!)
     ]
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var usernameSection = [String]()
+    var usernameDictionary = [String : [UserModel]]()
+    
+    func generateWordsDict(){
+        for username in friendsLocalUser {
+            
+            let key = "\(username.name[username.name.startIndex])".lowercased()
+            
+            if var usernameValue = usernameDictionary[key]
+            {
+                usernameValue.append(username)
+            }else{
+                usernameDictionary[key] = [username]
+            }
+        }
+        usernameSection = [String](usernameDictionary.keys)
+        usernameSection = usernameSection.sorted()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateWordsDict()
+        let nav = self.navigationController?.navigationBar
+        nav?.tintColor = UIColor(red: 16.0, green: 18.0, blue: 34.0, alpha: 1.0)
+        nav?.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        print(usernameDictionary)
         tableView.dataSource = self
+        searchBar.delegate = self
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.resignFirstResponder()
+        }
+        
     }
 
     // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friendsLocalUser.count
+    
+   override func numberOfSections(in tableView: UITableView) -> Int {
+        return usernameSection.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return usernameSection[section].uppercased()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let contactKey = usernameSection[section]
+        if let contactValue = usernameDictionary[contactKey]{
+            return contactValue.count
+        }
+        return 0
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: FriendsCell.reuseIdentifier, for: indexPath) as? FriendsCell else { return UITableViewCell()}
-        let imageHeight: CGFloat = 35, imageWidth: CGFloat = 35
-        let xPosition = (cell.friendImageView.frame.width / 2) - (imageWidth / 2)
-        let yPosition = (cell.friendImageView.frame.height / 2) - (imageHeight / 2)
-        let shadowSubview = UIView(frame: CGRect(x: xPosition, y: yPosition, width: imageWidth, height: imageHeight))
-        shadowSubview.clipsToBounds = false
-        shadowSubview.layer.shadowColor = cell.shadowColor?.cgColor
-        shadowSubview.layer.shadowOpacity = 1
-        shadowSubview.layer.shadowOffset = CGSize(width: 0, height: 0)
-        shadowSubview.layer.shadowRadius = cell.shadowRadius
-        shadowSubview.layer.shadowPath = UIBezierPath(roundedRect: shadowSubview.bounds, cornerRadius: cell.friendImageView.frame.size.width / 2).cgPath
-
-        let roundSubview = UIImageView(frame: shadowSubview.bounds)
-        roundSubview.clipsToBounds = true
-        roundSubview.layer.cornerRadius = cell.friendImageView.frame.size.width / 2
-        roundSubview.layer.backgroundColor = UIColor.white.cgColor
-        roundSubview.image = friendsLocalUser[indexPath.row].avatar
         
-        cell.friendImageView.addSubview(shadowSubview)
-        shadowSubview.addSubview(roundSubview)
+        let usernameKey = usernameSection[indexPath.section]
         
-        cell.friendName.text = friendsLocalUser[indexPath.row].name
+        if  let usernameValue = usernameDictionary[usernameKey.lowercased()] {
+            cell.friendName?.text = usernameValue[indexPath.row].name
+            let imageHeight: CGFloat = 35, imageWidth: CGFloat = 35
+            let xPosition = (cell.friendImageView.frame.width / 2) - (imageWidth / 2)
+            let yPosition = (cell.friendImageView.frame.height / 2) - (imageHeight / 2)
+            let shadowSubview = UIView(frame: CGRect(x: xPosition, y: yPosition, width: imageWidth, height: imageHeight))
+            shadowSubview.clipsToBounds = false
+            shadowSubview.layer.shadowColor = cell.shadowColor?.cgColor
+            shadowSubview.layer.shadowOpacity = 1
+            shadowSubview.layer.shadowOffset = CGSize(width: 0, height: 0)
+            shadowSubview.layer.shadowRadius = cell.shadowRadius
+            shadowSubview.layer.shadowPath = UIBezierPath(roundedRect: shadowSubview.bounds, cornerRadius: cell.friendImageView.frame.size.width / 2).cgPath
 
+            let roundSubview = UIImageView(frame: shadowSubview.bounds)
+            roundSubview.clipsToBounds = true
+            roundSubview.layer.cornerRadius = cell.friendImageView.frame.size.width / 2
+            roundSubview.layer.backgroundColor = UIColor.white.cgColor
+            roundSubview.image = usernameValue[indexPath.row].avatar
+            
+            cell.friendImageView.addSubview(shadowSubview)
+            shadowSubview.addSubview(roundSubview)
+        }
+        
         return cell
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return usernameSection
+    }
+    
+    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        guard let index = usernameSection.firstIndex(of: title) else {
+            return -1
+        }
+        return index
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
     }
 }
